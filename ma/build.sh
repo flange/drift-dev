be51b39593bc5fda2ffaa6327fe0e7c77fb790f7
Frank Lange <langefrq@informatik.hu-berlin.de>
2017-02-10 17:30:40 +0100

    MA:   * started chapter 3: related work

--- ma/build.sh
+++ ma/build.sh
@@ -6,13 +6,11 @@ cp bibtex/* build
 
 cd build
 
-pdflatex 0_main.tex
+pdflatex -draftmode 0_main.tex
 bibtex 0_main
+pdflatex -draftmode 0_main.tex
 pdflatex 0_main.tex
-pdflatex 0_main.tex
-
 
 if [ $? == 0 ]; then
-  echo "yo"
   cp 0_main.pdf ../ma.pdf
 fi
